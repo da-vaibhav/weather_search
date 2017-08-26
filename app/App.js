fee47b1169c92b1461bf0fd35e30f49d06172a66
vaibhav <vaibhav.vgc@gmail.com>
2017-08-26 22:58:31 +0530

    eslint fix, removed propTypes

--- app/App.js
+++ app/App.js
@@ -1,23 +1,23 @@
 /* global alert */
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import { SetUserLocation,
-         SearchQueryChange,
-         IsLoading,
-         OnFormSubmit
-       } from './actions';
+  SearchQueryChange,
+  IsLoading,
+  OnFormSubmit,
+} from './actions';
 
 import WeatherList from './WeatherList';
 import UserResultsSection from './UserResultsSection';
 import CityData from './CityData';
 import { requestUsersLocation,
-         SaveToLocalStorage,
-         fetchGeoData
-       } from './utils';
+  SaveToLocalStorage,
+  fetchGeoData,
+} from './utils';
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     // bind methods
@@ -28,34 +28,39 @@ class App extends Component {
     this.null_search_text = 'please enter city names and press enter to search for cities.';
   }
 
-  getUserLocation (e) {
+  onFormSubmit(e) {
+    e.preventDefault();
+    this.props.dispatch(OnFormSubmit(this.props.SearchQuery));
+  }
+
+  getUserLocation(e) {
     e.preventDefault();
     console.log('getting User Location...');
 
     // handle rejection here
     requestUsersLocation()
-    .then(({lat, lon}) => {
-      console.log('got user location ', lat, lon);
-      this.props.dispatch(IsLoading(true));
-      return fetchGeoData(lat, lon);
-    })
-    .then(data => {
-      console.log('data list => ', data.list);
-      SaveToLocalStorage('forecast_data', data.list);
+      .then(({ lat, lon }) => {
+        console.log('got user location ', lat, lon);
+        this.props.dispatch(IsLoading(true));
+        return fetchGeoData(lat, lon);
+      })
+      .then((data) => {
+        console.log('data list => ', data.list);
+        SaveToLocalStorage('forecast_data', data.list);
 
-      this.props.dispatch(SetUserLocation({
-        lat: data.city.coord.lat,
-        lon: data.city.coord.lon,
-        data: data.list
-      }));
-    })
-    .catch(err => {
-      alert(err);
-    });
+        this.props.dispatch(SetUserLocation({
+          lat: data.city.coord.lat,
+          lon: data.city.coord.lon,
+          data: data.list,
+        }));
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }
 
-  showUserLocationWeather () {
-    let LocalData = JSON.parse(window.localStorage.getItem('forecast_data'));
+  showUserLocationWeather() {
+    const LocalData = JSON.parse(window.localStorage.getItem('forecast_data'));
 
     if (LocalData.length < 1) {
       alert('Please provide your location to show the weather forecast data');
@@ -64,26 +69,21 @@ class App extends Component {
     console.log(LocalData);
   }
 
-  onFormSubmit (e) {
-    e.preventDefault();
-    this.props.dispatch(OnFormSubmit(this.props.SearchQuery));
-  }
-
-  locationChange (e) {
+  locationChange(e) {
     this.props.dispatch(SearchQueryChange(e.target.value));
   }
 
-  render () {
-    let { lat: Lat,
-          lon: Lon,
-          isLocationSet: isLocationAvailable,
-          citiesDataAvailable,
-          UserGeoData,
-          CitiesData: cities,
-          loading
-        } = this.props;
+  render() {
+    const { lat: Lat,
+      lon: Lon,
+      isLocationSet: isLocationAvailable,
+      citiesDataAvailable,
+      UserGeoData,
+      CitiesData: cities,
+      loading,
+    } = this.props;
 
-    let locationAvailability = isLocationAvailable ? `Lat: ${Lat}, Lon: ${Lon}` : 'Not Available';
+    const locationAvailability = isLocationAvailable ? `Lat: ${Lat}, Lon: ${Lon}` : 'Not Available';
 
     let SearchResults;
 
@@ -91,9 +91,9 @@ class App extends Component {
       SearchResults = cities.map((city, i) => <CityData key={i} city_data={city} />);
     }
 
-    let WeatherForUser = UserGeoData.map((day, i) =>
-      <WeatherList key={i} index={i} day={day} />
-   );
+    const WeatherForUser = UserGeoData.map((day, i) =>
+      <WeatherList key={i} index={i} day={day} />,
+    );
 
     return (
       <form onSubmit={this.onFormSubmit}>
@@ -103,30 +103,33 @@ class App extends Component {
           </pre>
         */}
 
-        <input value={this.props.SearchQuery} type='search'
+        <input
+          value={this.props.SearchQuery}
+          type="search"
           onChange={this.locationChange}
-          placeholder='Enter city names separated by comma. Eg. Mumbai, Pune, Nagpur' />
+          placeholder="Enter city names separated by comma. Eg. Mumbai, Pune, Nagpur"
+        />
         {' '}
 
-        <button type='submit'>Search</button>
+        <button type="submit">Search</button>
         <span> Your Location: {locationAvailability} </span>
-        <button type='button' onClick={this.getUserLocation}>
+        <button type="button" onClick={this.getUserLocation}>
           Show weather for my location
         </button>
 
         {
           !citiesDataAvailable
-          ? (<p><small>{this.null_search_text}</small></p>)
-          : null
+            ? (<p><small>{this.null_search_text}</small></p>)
+            : null
         }
 
-        {loading ? <div className='loading-spinner'></div> : ''}
+        {loading ? <div className="loading-spinner" /> : ''}
         {isLocationAvailable ? <UserResultsSection WeatherForUser={WeatherForUser} /> : null}
 
         {
           citiesDataAvailable
-          ? (<div className='city-container'>{SearchResults}</div>)
-          : null
+            ? (<div className="city-container">{SearchResults}</div>)
+            : null
         }
 
       </form>
@@ -134,7 +137,7 @@ class App extends Component {
   }
 }
 
-function matchStateToProps (state) {
+function matchStateToProps(state) {
   return {
     SearchQuery: state.SearchQuery,
     lat: state.location.latitude,
@@ -143,19 +146,8 @@ function matchStateToProps (state) {
     UserGeoData: state.location.UserGeoData,
     CitiesData: state.CitiesData,
     loading: state.IsLoading,
-    citiesDataAvailable: state.QueryDataFetched
+    citiesDataAvailable: state.QueryDataFetched,
   };
 }
-
-App.propTypes = {
-  SearchQuery: React.PropTypes.string,
-  lat: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
-  lon: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
-  isLocationSet: React.PropTypes.bool,
-  UserGeoData: React.PropTypes.array,
-  CitiesData: React.PropTypes.array,
-  loading: React.PropTypes.bool,
-  citiesDataAvailable: React.PropTypes.bool
-};
 
 export default connect(matchStateToProps)(App);
--- app/WeatherList.js
+++ app/WeatherList.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
-var WeatherList = (props) => {
-  return (
-    <li>
-      <span>day {props.index + 1}</span> {props.day.weather[0].description}
-    </li>
-  );
-};
-
-WeatherList.propTypes = {
-  index: React.PropTypes.number
-};
+const WeatherList = props => (
+  <li>
+    <span>day {props.index + 1}</span> {props.day.weather[0].description}
+  </li>
+);
 
 export default WeatherList;
